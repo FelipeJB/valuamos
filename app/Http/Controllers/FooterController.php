56f673e3dac5408fe8a5a33aa6343df925a1a565
FelipeJB <felipejbq@hotmail.com>
2018-07-05 19:59:41 -0500

    Added controller to delete links

--- app/Http/Controllers/FooterController.php
+++ app/Http/Controllers/FooterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
 use App\datosContacto;
-use App\vinculo;
+use App\vinculos;
 use Auth;
 use Redirect;
 
@@ -29,6 +29,24 @@ class FooterController extends Controller
                   return Redirect::to('/admin/footer')->with("SuccessDatos", "Se actualizaron los datos de contacto");
           }
           return Redirect::to('/admin/footer')->with("ErrorDatos", "Por favor ingrese todos los campos");
+      }
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function deleteVinculo()
+  {
+      if (Auth::check()) {
+        //verificar si hay Vinculos
+        if(count(vinculos::all())==0){
+          return Redirect::to('/admin/footer')->with("ErrorVinculo", "No hay vínculos para eliminar");
+        }
+        $vinculo=vinculos::where('id','=',Input::get('SelDeleteVinculo'))->get()[0];
+        $vinculo->delete();
+        return Redirect::to('/admin/footer')->with("SuccessVinculo", "Se elimin&oacute el vínculo");
       }
   }
 

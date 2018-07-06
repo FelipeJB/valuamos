c95001f2ecde094e7f47b0b55bed1dc49870f5c6
FelipeJB <felipejbq@hotmail.com>
2018-07-05 20:41:57 -0500

    Added controller for editing links. Kink administration finished

--- app/Http/Controllers/FooterController.php
+++ app/Http/Controllers/FooterController.php
@@ -42,22 +42,57 @@ class FooterController extends Controller
     try {
       if (Auth::check()){
           if (Input::get('NombreAddVinculo')!=null && Input::get('NombreAddVinculo')!="" && Input::get('VinculoAddVinculo')!=null && Input::get('VinculoAddVinculo')!=""){
-                //agregar vínculo
-                $v = new vinculos();
-                $v->nombre = Input::get('NombreAddVinculo');
-                if (strpos(Input::get('VinculoAddVinculo'), 'http') === false) {
-                  $v->vinculo = 'http://' .Input::get('VinculoAddVinculo');
-                }else{
-                  $v->vinculo = Input::get('VinculoAddVinculo');
-                }
-                $v->icono = Input::get('IconoAddVinculo');
-                $v->save();
-                return Redirect::to('/admin/footer')->with("SuccessVinculo", "Se agreg&oacute el vínculo");
+            //agregar vínculo
+            $v = new vinculos();
+            $v->nombre = Input::get('NombreAddVinculo');
+            if (strpos(Input::get('VinculoAddVinculo'), 'http') === false) {
+              $v->vinculo = 'http://' .Input::get('VinculoAddVinculo');
+            }else{
+              $v->vinculo = Input::get('VinculoAddVinculo');
+            }
+            $v->icono = Input::get('IconoAddVinculo');
+            $v->save();
+            return Redirect::to('/admin/footer')->with("SuccessVinculo", "Se agreg&oacute el vínculo");
           }
           return Redirect::to('/admin/footer')->with("ErrorVinculo", "Por favor ingrese todos los campos");
       }
     } catch (Exception $e) {
       return Redirect::to('/admin/footer')->with("ErrorVinculo", "Error agregando el vínculo");
+    }
+  }
+
+  /**
+   * Show the form for editing resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function editVinculo()
+  {
+    try {
+      if (Auth::check()){
+        //verificar si hay vínculos
+        if(count(vinculos::all())!=0){
+          if (Input::get('NombreEditVinculo')!=null && Input::get('NombreEditVinculo')!="" && Input::get('VinculoEditVinculo')!=null && Input::get('VinculoEditVinculo')!=""){
+            //agregar vínculo
+
+            $v = vinculos::where('id','=',Input::get('SelEditVinculo'))->get()[0];
+            $v->nombre = Input::get('NombreEditVinculo');
+            if (strpos(Input::get('VinculoEditVinculo'), 'http') === false) {
+              $v->vinculo = 'http://' .Input::get('VinculoEditVinculo');
+            }else{
+              $v->vinculo = Input::get('VinculoEditVinculo');
+            }
+            $v->icono = Input::get('IconoEditVinculo');
+            $v->save();
+            return Redirect::to('/admin/footer')->with("SuccessVinculo", "Se actualiz&oacute el vínculo");
+
+          }
+          return Redirect::to('/admin/footer')->with("ErrorVinculo", "Por favor ingrese todos los campos");
+        }
+          return Redirect::to('/admin/footer')->with("ErrorVinculo", "No hay vínculos para editar");
+      }
+    } catch (Exception $e) {
+      return Redirect::to('/admin/footer')->with("ErrorVinculo", "Error editando el vínculo");
     }
   }
 
